03e8ff1b2b17389a2fda640c9fb53a10e71a4697
Anna Prusińska <anna.prusinska12@gmail.com>
2023-03-23 00:15:44 +0100

    init app

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className='py-3 my-4'>
+        <p className='text-center text-muted fixed-bottom'>
+          Copyright © PizzeriaApp 2023
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
